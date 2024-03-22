c8c77b2b8451435a62fc898cbde2414e3916cd81
Gamemon <33440226+Gamemon@users.noreply.github.com>
2024-03-22 10:18:53 -0500

    Update SearchForADoc.java

--- SearchForADoc.java
+++ SearchForADoc.java
@@ -1,1 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+public class SearchForADoc{
+  private String docName = "";
+  public SearchForADoc(String doc){
+    docName = doc;
+  }
+  public setTxtName(String doc){
+    docName = doc;
+  }
+  public void search(String specificDoc){
 
+    try{
+      BufferedReader br = new BufferedReader(docName);
+    } catch(FileNotFoundException e){
+      e.printStackTrace();
+    }
+    //Line by line
+    //Find # (indexOf)
+    //if none go to next line, if some see if matching, if not go to next line
+    //Finish if none left
+    
+    boolean checker = true;
+    while(checker){
+      
+    }
+  }
+
+
+}
