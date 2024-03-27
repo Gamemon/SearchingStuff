07f103225c42fda8bc141ffa4d51b7675aac4fb2
Gamemon <33440226+Gamemon@users.noreply.github.com>
2024-03-27 09:55:34 -0500

    Update SearchForAWord.java

--- SearchForAWord.java
+++ SearchForAWord.java
@@ -16,33 +16,19 @@ public class SearchForAWord extends SearchForADoc{
   }
   public void search(){
      BufferedReader objReader = null;
+    String strCurrentLine;
+    
   try {
-   String strCurrentLine;
-
    BufferedReader objReader = new BufferedReader(new FileReader(Title));
-
-   while ((strCurrentLine = objReader.readLine()) != null) {
-
-    System.out.println(strCurrentLine);
-   }
-
   } catch (IOException e) {
-
    e.printStackTrace();
+  } 
 
-  } finally {
-
-   try {
-    if (objReader != null)
-     objReader.close();
-   } catch (IOException ex) {
-    ex.printStackTrace();
-   }
-  }
+    objReader.close();
 }
 
 
-
+/*
 puplic class LinearSearch(){    
 public static int linearSearch(Title, String word){    
         for(String i=0;i<Title.length;i++){    
@@ -57,6 +43,7 @@ public static int linearSearch(Title, String word){
     }    
 
 }
+  */
 
   
 
