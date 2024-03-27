f6614971340ac6aa880006c9b5d5c2c952db4436
RonitWade12 <149329338+RonitWade12@users.noreply.github.com>
2024-03-27 10:26:57 -0500

    Update SearchForAWord.java
    HELP

--- SearchForAWord.java
+++ SearchForAWord.java
@@ -5,32 +5,74 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 public class SearchForAWord extends SearchForADoc{
-  private word;
-  private title;
-  public SearchForAWord(String word){
+  private word1;
+  private titleSyn;
+  public SearchForAWord(String word, String syn){
     word1 = word;
+    titleSyn = syn;
   }
-  public SearchForAWord(String word,String title){
-    word1 = word;
-    title1 = title;
-  }
-  public void search(){
-     BufferedReader objReader = null;
-    String strCurrentLine;
-    
-  try {
-   BufferedReader objReader = new BufferedReader(new FileReader(Title));
-  } catch (IOException e) {
-   e.printStackTrace();
-  } 
+  public void search(ArrayList<Article> arts){
+    String line = "";
+    try{
+      BufferedReader br = new BufferedReader(new FileReader(titleSyn));
+    } catch(FileNotFoundException e){
+      e.printStackTrace();
+    }
 
-    objReader.close();
+    //Find synonym line
+    while((line = br.readLine()) != null){
+      String tempWord = line.substring(0, line.indexOf(","));
+      if (tempWord.equals(word1)){
+        break;
+      }
+    }
+    br.close();
+
+    //create priorities
+    for (Article thingy : arts){
+      if (thingy.indexOf(word1) != -1){
+        thingy.setPriority(1);
+      } else {
+        boolean checker = true;
+        while(checker){
+          line = line.substring(line.indexOf(",")+1);
+          String tempWord = "";
+          if ((tempWord.equals(line.substring(0, line.indexOf(","))))){
+            thingy.setPriority(2);
+          }
+          if (line.indexOf(",") == -1){
+            checker = false;
+          }
+        }
+      }
+    }
+
+    //String concatenation with Article toString method
+   
+   
+   
+  }
+
+
+/*
+puplic class LinearSearch(){    
+public static int linearSearch(Title, String word){    
+        for(String i=0;i<Title.length;i++){    
+            if(Title[i] == word){    
+                return i;
+            }
+          for(String j=0;j){
+          }
+               
+        }    
+        return -1;    
+    }    
+
+}
+  */
+
+ 
+
+ 
 }
 
-
-
-
-  
-
-  
-}
